e860666c81fdaad1e49da1c8f6de31b84d28c319
Luigi Polidorio <polidorio_luigi@yahoo.com.br>
2020-07-28 17:45:45 -0400

    Graphql Upload

--- src/graphql/base.ts
+++ src/graphql/base.ts
@@ -1,13 +1,20 @@
-import {
-  gql
-} from "apollo-server-koa";
+import { gql } from "apollo-server-koa";
+import { GraphQLUpload } from "graphql-upload";
 
-export default gql `
-   type Query {
+export const typeDefs = gql`
+  type Query {
     _empty: String
   }
 
   type Mutation {
     _empty: String
   }
+
+  scalar Upload
 `;
+
+export const resolvers = {
+  resolvers: {
+    Upload: GraphQLUpload,
+  },
+};
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,13 +1,13 @@
 import { merge } from "lodash";
 import { makeExecutableSchema } from "apollo-server-koa";
-import Base from "./base";
+import * as Base from "./base";
 import Vendor from "../domains/vendors/graphql/typeDefs";
 import vendorResolvers from "../domains/vendors/graphql/resolvers";
 import Technology from "../domains/technologies/graphql/typeDefs";
 import technologyResolvers from "../domains/technologies/graphql/resolvers";
 
-const typeDefs = [Base, Vendor, Technology];
-const resolvers = merge(vendorResolvers, technologyResolvers);
+const typeDefs = [Base.typeDefs, Vendor, Technology];
+const resolvers = merge(Base.resolvers, vendorResolvers, technologyResolvers);
 
 const schema = makeExecutableSchema({
   typeDefs,
